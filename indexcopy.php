95275ecb9772c031a0569648cc4c1e36104c8351
Crazycatwastaken <60655579+Crazycatwastaken@users.noreply.github.com>
2022-05-25 09:44:03 +1000

    Add files via upload

---
+++ indexcopy.php
@@ -0,0 +1,16 @@
+<?php
+$dogage = rand(1,20);
+$DogInHuman;
+
+
+echo $dogage, "<br>";
+
+if ($dogage <= 2){
+    $DogInHuman = $dogage*12;
+} elseif  ($dogage > 2 ) {
+    $DogInHuman = 24+(($dogage-2)*(6));
+} 
+
+echo $DogInHuman, "<br>";
+echo "The dog is $DogInHuman year's old.";
+?>
